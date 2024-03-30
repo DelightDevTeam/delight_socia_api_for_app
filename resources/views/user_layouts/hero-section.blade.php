b556ee5c574149dbf77e2c23ecb16533b7eb0071
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-03-31 01:49:12 +0630

    apk download btn added

--- resources/views/user_layouts/hero-section.blade.php
+++ resources/views/user_layouts/hero-section.blade.php
@@ -3,6 +3,12 @@
         <div class="home-text container">
           <h2 class="home-title">Delight Myanmar Blog</h2>
           <span class="home-subtitle">You can see interesting things.</span>
+          <div class="text-center mt-3">
+            <a href="https://www.delightmyanmar.online/assets/app/delight.apk" target="_blank" class="btn btn-outline-light">
+              <i class="fas fa-download me-2"></i>
+              Download
+            </a>
+          </div>
         </div>
       </section>
       <!-- End Home -->
